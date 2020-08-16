0a34b539a4a9e985e51c518a7d89cd3334e6cd4d
Илларион Иванченко <larik.maths@outlook.com>
2020-08-16 19:46:53 +0300

    feat: jwt, graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { PostModule } from './post/post.module';
+import { JwtStrategy } from './users/jwt.strategy'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
-@Module({
-  imports: [
-    GraphQLModule.forRoot({
-      debug: false,
-      playground: false,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+const passportModule = PassportModule.register({
+	defaultStrategy: 'jwt'
 })
-export class AppModule {}
+@Module({
+	imports: [TypeOrmModule.forRoot(), JwtModule.register({
+		secretOrPrivateKey: '1AGy4bCUoECDZ4yI6h8DxHDwgj84EqStMNyab8nPChQ=',
+		signOptions: {
+			expiresIn: '12h'
+		}
+	}),
+	PassportModule.register({
+		defaultStrategy: 'jwt'
+	}), GraphQLModule.forRoot({
+		typePaths: ['./**/*.graphql'],
+		context: ({ req }) => ({ req })
+	}), passportModule,
+		UsersModule, PostModule],
+	controllers: [],
+	providers: [JwtStrategy],
+	exports: [
+		passportModule
+	]
+})
+export class AppModule { }
